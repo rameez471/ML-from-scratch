76c3c59f25a0a34a15bcd8d5ce663e8711b8ee47
rameez471 <yashpathak29@gmail.com>
2020-10-01 20:53:41 +0530

    Neural Network

--- Neural Network/NeuralNet.py
+++ Neural Network/NeuralNet.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 def activation(z,derivative=False):
     if derivative:
@@ -76,4 +79,92 @@ class NeuralNetwork(object):
                                                                                                      val_loss, 
                                                                                                      train_acc, 
                                                                                                      val_acc)
-        plt.title(message)                                                                                             
+        plt.title(message)  
+
+    def train(self,X,y,batch_size,epochs,learning_rate,validation_split=0.2,plot=False):
+
+        X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=validation_split)
+        X_train,X_test,y_train,y_test = X_train.T,X_test.T,y_train.T,y_test.T
+
+        history_train_losses = []
+        history_train_accuracies = []
+        history_test_losses = []
+        history_test_accuracies = []
+
+        for i in range(epochs):
+            if X_train.shape[1] % batch_size == 0:
+                n_batches = int(X_train.shape[1] / batch_size)
+            else:
+                n_batches = int(X_train.shape[1] / batch_size)-1
+            
+            X_train, y_train = shuffle(X_train.T, y_train.T)
+            X_train, y_train = X_train.T, y_train.T
+
+            batches_X = [X_train[:,batch_size*i:batch_size*(i+1)] for i in range(0,n_batches)]
+            batches_y = [y_train[:,batch_size*i:batch_size*(i+1)] for i in range(0,n_batches)]
+
+            train_losses = []
+            train_accuracies = []
+
+            test_losses = []
+            train_accuracies = []
+
+            dw_per_epoch = [np.zeros(w.shape) for w in self.weights]
+            db_per_epoch = [np.zeros(b.shape) for b in self.biases]
+            
+            for batch_x, batch_y in zip(batches_X,batch_y):
+                batch_y_pred,pre_activations,activations = self.forward(batch_x)
+                deltas = self.compute_deltas(pre_activations,batch_y,batch_y_pred)
+                dW, db = self.backpropagate(deltas, pre_activations, activations)
+
+                for i,(dw_i,db_i) in enumerate(zip(dW,db)):
+                    dw_per_epoch[i] += dw_i / batch_size
+                    db_per_epoch[i] += db_i / batch_size
+
+                batch_y_train_pred = self.predict(batch_x)
+
+                train_loss = cost_function(batch_y, batch_y_train_pred)
+                train_losses.append(train_loss)
+                train_accuracy = accuracy_score(batch_y.T, batch_y_train_pred.T)
+                train_accuracies.append(train_accuracy)
+
+                batch_y_test_pred = self.predict(X_test)
+
+                test_loss = cost_function(y_test, y_test_pred)
+                test_losses.append(test_loss)
+                test_accuracy = accuracy_score(y_test.T,batch_y_pred.T)
+                test_accuracies.append(test_accuracy)
+            
+            for i,(dw_epoch, db_epoch) in enumerate(zip(dw_per_epoch, db_per_epoch)):
+                self.weights[i] = self.weights[i] - learning_rate * dw_epoch
+                self.biases[i] = self.biases[i] - learning_rate * db_epoch
+
+            history_train_losses.append(np.mean(train_losses))
+            history_train_accuracies.append(np.mean(train_accuracies))
+            history_test_losses.append(np.mean(test_losses))
+            history_test_accuracies.append(np.mean(test_accuracies))
+
+        
+        self.plot_decision_regions(X, y,epochs,
+                                        np.round(np.mean(train_losses),4),
+                                        np.round(np.mean(test_losses),4),
+                                        np.round(np.mean(train_accuracies),4),
+                                        np.round(np.mean(test_accuracies),4))
+
+        history = {
+            'epochs':epochs,
+            'train_loss':history_train_losses,
+            'train_acc':history_train_accuracies,
+            'test_loss':history_test_losses,
+            'test_acc':history_test_accuracies
+        }
+
+    def predict(self,a):
+
+        for w,b in zip(self.weights,self.biases):
+            z = np.dot(w,a) + b
+            a = activation(z)
+        predictions = ( a > 0.5).astype(int)
+
+        return predictions
+
